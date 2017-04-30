64dd1352e6f3ba3afc0b525a2d103afbe938d68f
Javier Seixas <javier.seixas@scmspain.com>
2017-04-30 11:20:20 +0200

    Adds Settlement and Terrain

---
+++ src/conquerors/productionassistant/domain/settlement/Settlement.ts
@@ -0,0 +1,28 @@
+abstract class Settlement {
+
+    protected settlerDisposed : boolean = false;
+
+    constructor(readonly terrain : Terrain) {
+        this.terrain = terrain;
+    }
+
+    abstract upgrade(): Settlement;
+    abstract taxes(): Goods
+
+    disposeSettler(): void {
+
+        if (this.settlerDisposed) {
+            console.log("A settler has been already disposed in this settlement");
+            return;
+        }
+
+        // TODO rest from the reserve
+
+        this.settlerDisposed = true
+    }
+
+    production() : Goods {
+        return this.terrain.produce(this)
+    }
+
+}
---
+++ src/conquerors/productionassistant/domain/terrain/Terrain.ts
@@ -0,0 +1,5 @@
+interface Terrain {
+
+    produce(settlement: Settlement): Goods;
+
+}
