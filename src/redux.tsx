77172fce52a0ae0df683c298b3b07d364856c0ea
Javier Seixas <javier.seixas@scmspain.com>
2017-07-02 21:47:18 +0200

    React + Redux Tutorial #4: Multiple Reducers with Redux Reducers

--- src/redux.tsx
+++ src/redux.tsx
@@ -1,22 +1,48 @@
-import {createStore} from "redux";
+import {createStore, Reducer, ReducersMapObject, combineReducers} from "redux";
 
-const reducer = function (state : any, action : any) {
-    if (action.type === "INC") {
-        return state + 1;
+export class Tweet {
+
+}
+
+export class User {
+    public name: string;
+    public age: number;
+    tweets: Array<Tweet>;
+}
+
+const userReducer: Reducer<any> = (state : User = new User(), action: any) => {
+    switch (action.type) {
+        case "CHANGE_NAME": {
+            state = {...state, name: action.payload};
+            break;
+        }
+        case "CHANGE_AGE": {
+            state = {...state, age: action.payload};
+            break;
+        }
     }
-
     return state;
 };
 
-const store = createStore(reducer, 0);
+
+const tweetsReducer: Reducer<any> = (state : object = [], action: any) => {
+    return state;
+};
+
+const reducerMapObjects: ReducersMapObject = {
+    user: userReducer,
+    tweets: tweetsReducer
+};
+
+const reducers: any = combineReducers(reducerMapObjects);
+
+
+const store = createStore(reducers, {});
 
 store.subscribe(() => {
     console.log("store changed ", store.getState());
 });
 
-store.dispatch({type: "INC", payload: 1});
-store.dispatch({type: "INC", payload: 1});
-store.dispatch({type: "INC", payload: 1});
-store.dispatch({type: "INC", payload: 1});
-
+store.dispatch({type: "CHANGE_NAME", payload: "Will"});
+store.dispatch({type: "CHANGE_AGE", payload: 35});
 
